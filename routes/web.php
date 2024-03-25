121956895048bbb68d02dc03027704af858eb260
niko <shakilrehan23@gmail.com>
2024-03-25 16:45:20 +0530

    Added laravel breeze authentication

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource("/admin/products", ProductController::class);
 });
 
-Route::resource('/products', ProductController::class);
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
